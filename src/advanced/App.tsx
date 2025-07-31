b45d3cfa982bb8f0b27e6e7719f04eafa1ebbfae
HYOJIN <hyo.khj@gmail.com>
2025-08-01 00:35:02 +0900

    chore: react+typescript 세팅

---
+++ src/advanced/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Advanced Cart - React TypeScript</h1>
+        <p>장바구니 애플리케이션 (React + TypeScript)</p>
+      </header>
+      <main>
+        {/* 여기에 장바구니 컴포넌트들이 추가될 예정 */}
+        <p>개발 중...</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
