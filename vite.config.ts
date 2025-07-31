6a3f6d6041053fd80b89d115fdf3039139b52b04
HYOJIN <hyo.khj@gmail.com>
2025-08-01 06:15:17 +0900

    배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@/components': path.resolve(__dirname, './src/components'),
-      '@/hooks': path.resolve(__dirname, './src/hooks'),
-      '@/utils': path.resolve(__dirname, './src/utils'),
-      '@/types': path.resolve(__dirname, './src/types'),
-      '@/constants': path.resolve(__dirname, './src/constants'),
+  root: ".",
+  base: "/front_6th_chapter2-1/",
+  build: {
+    rollupOptions: {
+      input: "./index.html",
     },
+    outDir: "dist",
+    copyPublicDir: true,
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
   },
-}) 
+});
